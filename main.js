ce6195de9ba3bf7d4067f49dc9894ccf59f61603
Brian Vanegas <bvanegas18@hotmail.com>
2021-05-30 10:04:19 -0500

    Se crea el constructor para agregar las barras, se hace prueba

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Funcion anonima del primer objeto del Board
+//Funcion anonima del primer objeto modelo del Board
 (function(){
     self.Board = function(width, height){
         this.width = width;
@@ -9,17 +9,41 @@
         this.ball = null;
     }
 
-    //Se crea para obtener los prototipos Getter
+    //Se crea para obtener los prototipos Getter de la clase Board
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
-//Clase para dibujar las vistas del pizarron
+//Constructor de la clase Bar
+(function(){
+    self.Bar = function(x,y,width, height, board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this);
+
+        this.kind = "rectangle";
+    }
+
+    self.Bar.prototype = {
+        down: function(){
+
+        },
+        up: function(){
+
+        }
+    }
+})();
+
+//Clase para dibujar las vistas del board
 (function(){
     self.BoardView = function(canvas,board){
         this.canvas = canvas;
@@ -27,6 +51,26 @@
         this.canvas.height = board.height;
         this.board = board;
         this.context = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype={
+        draw: function(){
+            for (var i = this.board.elements.length -1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.context,el);
+            };
+        }
+    }
+
+    function draw(context,element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    context.fillRect(element.x,element.y, element.width,element.height);
+                    break;
+            }
+        }        
     }
 })();
 
@@ -36,6 +80,10 @@ window.addEventListener("load",main);
 function main()
 {
     var board = new Board(800,400);
+    var bar = new Bar(20,100,20,100,board);
+    var bar = new Bar(760,100,20,100,board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas,board);
+
+    board_view.draw();
 }
